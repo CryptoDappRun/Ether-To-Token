f6789bca6e182c340be2f97498eef0aa5678aa82
CryptoDappRun <122711721+CryptoDappRun@users.noreply.github.com>
2023-02-05 11:14:30 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,11 +3,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var BasecoinName= "BNB";
-var TokenName="Moon"
+var BasecoinName= "BNB"; 
+var TokenName="Moon"      //your token name
 
-var ChainID= 56;
-var ChainName= "Binance";
-var BlockchainScanURL="https://bscscan.com/tx/";
-var contractAdress="0xD32F1544c36D0942De599Bb1c8b36F940e2f9853"
-var TokenAddress  ="0x0F6FC13d26EeA45d3cdA81bCBcb0b7bE711BA3fE"
+var ChainID= 56;          //find this on https://cryptodapprun.github.io/Blockchain-To-Metamask/
+var ChainName= "Binance"; //find this on  https://cryptodapprun.github.io/Blockchain-To-Metamask/
+
+
+var BlockchainScanURL="https://bscscan.com/tx/";  //blockchain RPC url 
+var contractAdress="0xD32F1544c36D0942De599Bb1c8b36F940e2f9853"  //deploy contract/SwapToken.sol,then you will get the address
+var TokenAddress  ="0x0F6FC13d26EeA45d3cdA81bCBcb0b7bE711BA3fE"  //your token address
